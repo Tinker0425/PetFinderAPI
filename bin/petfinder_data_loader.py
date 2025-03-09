6a473c2f4ada0e9ae0c04ea0d0be5b0d32c24766
Flynn25 <Kayla425@users.noreply.github.com>
2025-03-09 14:29:14 -0800

    :bug: daily yml hardcode

--- bin/petfinder_data_loader.py
+++ bin/petfinder_data_loader.py
@@ -136,7 +136,7 @@ def main():
     # Load secrets from environment variables
     client_id = os.getenv("PETFINDER_CLIENT_ID")
     client_secret = os.getenv("PETFINDER_CLIENT_SECRET")
-    bucket_name = "myapi-petfinder-data"
+    bucket_name = "petfinderapi-petfinder-data"
     credentials_json = os.getenv("GCS_CREDENTIALS")
 
     if not client_id or not client_secret or not bucket_name or not credentials_json:
